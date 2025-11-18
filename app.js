cfa091905712ad6581038f64cdbb90452682b0ee
krg442-coder <krg442@nau.edu>
2025-11-17 18:06:31 -0700

    app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// Each of the toes and their own alerts
+document.getElementById("toe1").onclick = () => 
+  alert("That is ABA!");
+document.getElementById("toe2").onclick = () => 
+  alert("May Guilty Gear!");
+document.getElementById("toe3").onclick = () => 
+  alert("Ky Kiske!");
+document.getElementById("toe4").onclick = () => 
+  alert("Lucy Cyberpunk!");
+
+// Link to Website
+document.getElementById("palm").onclick = () =>
+  window.location.href = "https://www.guiltygear.com/ggst/en/";

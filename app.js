a281627131ffdf0626a4b6cde5516983303a1cef
hkn2-9 <hkn33@nau.edu>
2025-12-02 15:09:51 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -12,3 +12,73 @@ document.getElementById("toe4").onclick = () =>
 document.getElementById("palm").onclick = () =>
   window.location.href = "https://www.coconinohumane.org/";
 
+// Each of the toes and their own alerts
+document.getElementById("paw2-toe1").onclick = () => 
+  alert("That is ABA!");
+document.getElementById("paw2-toe2").onclick = () => 
+  alert("May Guilty Gear!");
+document.getElementById("paw2-toe3").onclick = () => 
+  alert("Ky Kiske!");
+document.getElementById("paw2-toe4").onclick = () => 
+  alert("Lucy Cyberpunk!");
+
+// Link to Website
+document.getElementById("paw2-palm").onclick = () =>
+  window.location.href = "https://www.coconinohumane.org/";
+
+// Each of the toes and their own alerts
+document.getElementById("paw3-toe1").onclick = () => 
+  alert("That is ABA!");
+document.getElementById("paw3-toe2").onclick = () => 
+  alert("May Guilty Gear!");
+document.getElementById("paw3-toe3").onclick = () => 
+  alert("Ky Kiske!");
+document.getElementById("paw3-toe4").onclick = () => 
+  alert("Lucy Cyberpunk!");
+
+// Link to Website
+document.getElementById("paw3-palm").onclick = () =>
+  window.location.href = "https://www.coconinohumane.org/";
+
+// Each of the toes and their own alerts
+document.getElementById("paw4-toe1").onclick = () => 
+  alert("That is ABA!");
+document.getElementById("paw4-toe2").onclick = () => 
+  alert("May Guilty Gear!");
+document.getElementById("paw4-toe3").onclick = () => 
+  alert("Ky Kiske!");
+document.getElementById("paw4-toe4").onclick = () => 
+  alert("Lucy Cyberpunk!");
+
+// Link to Website
+document.getElementById("paw4-palm").onclick = () =>
+  window.location.href = "https://www.coconinohumane.org/";
+
+// Each of the toes and their own alerts
+document.getElementById("paw5-toe1").onclick = () => 
+  alert("That is ABA!");
+document.getElementById("paw5-toe2").onclick = () => 
+  alert("May Guilty Gear!");
+document.getElementById("paw5-toe3").onclick = () => 
+  alert("Ky Kiske!");
+document.getElementById("paw5-toe4").onclick = () => 
+  alert("Lucy Cyberpunk!");
+
+// Link to Website
+document.getElementById("paw5-palm").onclick = () =>
+  window.location.href = "https://www.coconinohumane.org/";
+
+// Each of the toes and their own alerts
+document.getElementById("paw6-toe1").onclick = () => 
+  alert("That is ABA!");
+document.getElementById("paw6-toe2").onclick = () => 
+  alert("May Guilty Gear!");
+document.getElementById("paw6-toe3").onclick = () => 
+  alert("Ky Kiske!");
+document.getElementById("paw6-toe4").onclick = () => 
+  alert("Lucy Cyberpunk!");
+
+// Link to Website
+document.getElementById("paw6-palm").onclick = () =>
+  window.location.href = "https://www.coconinohumane.org/";
+

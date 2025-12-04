5ac19783886a190dc845b79a560bebedf52ca563
krg442-coder <krg442@nau.edu>
2025-12-03 17:58:51 -0700

    Updated app.js

--- app.js
+++ app.js
@@ -82,4 +82,17 @@ document.getElementById("paw6-toe4").onclick = () =>
 document.getElementById("paw6-palm").onclick = () =>
   window.location.href = "https://yhbr.org/";
 
+// Each of the toes and their own alerts
+document.getElementById("product-toe1").onclick = () => 
+  alert("For dogs; this shampoo is a strong starter! Regardless of your pets sensitivity, a product taking the consideration of your friends well being is very important, and this does a perfect job!");
+document.getElementById("product-toe2").onclick = () => 
+  alert("An alternative cat shampoo from Anicura. While elevated in price, it strives on using natural ingredients to clean your friend, keeping in mind their sensitivity and well being.");
+document.getElementById("product-toe3").onclick = () => 
+  alert("Royal Canin cat food, while being primarily for outdoor cats, this food is packed with nutrition to keep your cat healthy as they go through the say. It is high quality for its price, and has a positive impact with its high-energy calorie content.");
+document.getElementById("product-toe4").onclick = () => 
+  alert("Royal Canin's Mini Adult dog food is a carefully managed kibble, adjusted to help raise the energy of your furry friend. While this is one choice of dog food, Royal Canin sells many conditional foods depending on your dogs' age, maintaining their health and well-being.");
+
+// Link to Website
+document.getElementById("product-palm").onclick = () =>
+  window.location.href = "https://www.royalcanin.com/uk";
 
